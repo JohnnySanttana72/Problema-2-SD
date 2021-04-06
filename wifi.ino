ee44cbaed29ad2e5be7d9631c32db0ada92a7baf
JohnnySanttana72 <33816846+JohnnySanttana72@users.noreply.github.com>
2021-04-06 18:56:47 -0300

    conectando NodeMCU a rede wifi
    conectando o NodeMCU a rede wifi, e retornado uma pagina HTML.

---
+++ wifi.ino
@@ -0,0 +1,92 @@
+#include <ESP8266WiFi.h> 
+
+const char* ssid = "***";
+const char* password = "***"; 
+
+WiFiServer server(80);
+int status_LED = LOW; 
+
+void setup()
+{
+  Serial.begin(115200); // Inicializa a serial
+ 
+  Serial.println();   // Pula uma linha na janela da serial
+     
+  pinMode(2, OUTPUT);              
+  digitalWrite(2, LOW);
+  
+  WiFi.begin(ssid, password); //Passa os parâmetros para a função que vai fazer a conexão com a rede sem fio
+  delay(1000); // Intervalo de 1000 milisegundos
+  Serial.print("Conectando"); // Escreve um texto na serial
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500); // Intervalo de 500 milisegundos
+    Serial.print("."); //Escreve o caractere na serial
+  }
+  
+  Serial.println();
+  server.begin();  //Inicia o Servidor para receber dados na porta definida em "WiFiServer server(porta)"
+  Serial.print("Endereço IP: "); // Escreve na janela da serial
+  
+  Serial.println(WiFi.localIP()); // Escreve na  o IP recebido dentro da rede sem fio (recebido de forma automática)
+  
+  delay(1000); // Intervalo de 1000 milisegundos
+
+}
+
+void loop() {
+  
+  WiFiClient client = server.available();  //Verifica se algum Cliente está conectado no Servidor   
+  
+  if (!client) {  // Se não existir Cliente conectado, faz
+    return; // Reexecuta o processo até que algum Cliente se conecte ao Servidor
+  }
+
+  while(!client.available()){ // Enquanto o Cliente estiver conectado
+    delay(1); //Intervalo de um milisegundo
+  }
+  
+  String request = client.readStringUntil('\r');  //Faz a leitura da primeira linha da Requisição
+  Serial.println("Nome da Requisicao");
+  Serial.println(request); //Escreve a requisição na serial
+  client.flush(); //Aguarda até que todos os dados de saída sejam enviados ao cliente
+ 
+
+  if (request.indexOf('/LED=ON') != -1){
+    status_LED = LOW;
+  } else if (request.indexOf('/LED=OFF') != -1){
+    status_LED = HIGH;
+  }
+ 
+  digitalWrite(2, status_LED);
+ 
+
+  client.println("HTTP/1.1 200 OK"); //Escreve para o cliente a versão do HTTP
+  client.println("Content-Type: text/html"); //Escreve para o cliente o tipo de conteúdo (texto/Html)
+  client.println("");
+
+  client.println("<!DOCTYPE HTML>"); //Informa ao navegador a especificação do Html
+  client.println("<html>"); //Abre a tag "html"
+  client.println("<head>"); //Abre o Head
+  client.println("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>");
+  client.println("</head>"); //Fecha o Head
+  client.println("<h1><center>Ola cliente!</center></h1>"); //Escreve "Ola cliente!" na página
+  client.println("<center><font size='5'>SD - Sistemas Digitais</center>"); //Escreve "Seja bem vindo!" na página
+  client.println();
+
+ // client.println("<h3>Dispositivo Conectado</>");
+  
+  client.println();
+  client.println("<div>");  
+   if(status_LED == HIGH)  {
+    client.print("<input type='image' src='https://thumbs.dreamstime.com/b/ícone-do-interruptor-de-ligar-desligar-no-fundo-branco-115192535.jpg' style='display:block; margin:auto' width='30%' onClick=location.href='/LED=ON'>");
+   } else {
+    client.print("<input type='image' src='https://thumbs.dreamstime.com/b/ícone-do-interruptor-de-ligar-desligar-no-fundo-branco-115194802.jpg' style='display:block; margin:auto' width='30%' onClick=location.href='/LED=OFF'>");
+   }
+ client.println("</div>");
+ 
+ client.println("</html>");  //Fecha a tag "html"
+ delay(1); //Intervalo de 1 milisegundo 
+ Serial.println(""); //Pula uma linha na janela serial
+}
