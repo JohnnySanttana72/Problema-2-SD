2c702361fcf591cd7adab0fda06b34f8636cbcfe
Rafael Brito <rafa.brito295@gmail.com>
2021-04-10 14:01:51 -0300

    Libraries, certificates e comunicação mqtt

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Problema-2-SD
+
+## Diretorios
+### 1 - libraries
+
+Contém as bibliotecas necessárias para executar o código no NodeMCU ESP8266.
+
+#### 1.1 - NTPClient.zip
+	
+Usado para se conectar a um servidor de horário NTP para se mantenha sincronizado com o horário da rede.
+
+#### 1.2 - PubSubClient.zip
+É uma biblioteca de cliente para mensagens MQTT.
+
+### 2 - wifi 
+
+####2.1 - Diretório data
+
+#####2.1.1 - wifi_credential_example.txt
+
+É um arquivo para configuração das credenciais 'nome_rede_wifi' e 'senha_da_rede_wifi' da rede Wifi. O arquivo devera ser renomeado para 'wifi_credential.txt', para ser possível ser lido.
+
+####2.2 - certificates_example.h
+
+É um arquivo para configur os certificados gerados na Aws deverá ser renomeado para 'certificates.h'. O conteúdo dos certificados respectivos(Amazon root CA certificado, A chave privada do dispositivo, O certificao para o dispositivo) deverão ser atribuído as seguintes constates que estão no arquivo .h:
+	
+	- AWS_CERT_CA;
+	- AWS_KEY_PRIVATE;
+	- AWS_CERT_CRT.
+
+####2.3 - wifi.ino
+
+É o código-fonte que será carregado no NodeMCU ESP8266 para o acionamento de uma LED usando o protocolo MQTT;
+
+Obs:. O endpoint para acesso a AWS IoT dentro do código-fonte deve ser definido. 
+
--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,13 +1,53 @@
-#include <ESP8266WiFi.h> 
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h> 
 #include <FS.h>
+#include "certificates.h"
+extern "C" {
+#include "libb64/cdecode.h"
+}
 
 const char* ssid;
-const char* password; 
+const char* password;
+const char* endpoint_aws = "seu-endpoint.iot.us-east-1.amazonaws.com"; // AWS Endpoint
 
-WiFiServer server(80);
-int status_LED = LOW; 
+long lastMsg = 0;
+char msg[50];  //buffer para conter a mensagem a ser publicada
+int count_send = 0;
+int timeZone = -3;
 
-void config_wifi(String path) {
+//Socket UDP que a biblioteca utiliza para recuperar dados sobre o horário
+WiFiUDP ntpUDP;
+
+//Objeto responsável por recuperar dados sobre horário
+NTPClient ntpClient(
+    ntpUDP,                 //socket udp
+    "0.br.pool.ntp.org",    //URL do servidor NTP
+    timeZone*3600,          //Deslocamento do horário em relacão ao GMT 0
+    60000);
+
+WiFiClientSecure espClient;
+
+/*WiFiServer server(80);
+int status_LED = LOW; */
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) 
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+PubSubClient client_pubsub(endpoint_aws, 8883, callback, espClient); //Começamos conectando a um número de porta MQTT de conjunto de rede WiFi para 8883 conforme padrão
+
+void config_wifi(String path) 
+{
   String ssid_wifi = "";
   String password_wifi = "";
   int count = 0;
@@ -32,6 +72,69 @@ void config_wifi(String path) {
   
   ssid = ssid_wifi.c_str(); //conversão de string para const char
   password = password_wifi.c_str(); //conversão de string para const char
+
+  Serial.println(ssid);
+  Serial.println(password);
+}
+
+void setupNTP()
+{
+    //Inicializa o client NTP
+    ntpClient.begin();
+    
+    //Espera pelo primeiro update online
+    Serial.println("Esperando pela primeira atualização");
+    while(!ntpClient.update())
+    {
+        ntpClient.forceUpdate();
+    }
+
+    // É usado para setar no espClient o tempo do servidor NTP para validar o certificado X509 e estabelecer conexão com o servidor aws
+    espClient.setX509Time(ntpClient.getEpochTime());
+}
+
+int b64decode(String b64Text, uint8_t* output) 
+{
+  base64_decodestate s;
+  base64_init_decodestate(&s);
+  int cnt = base64_decode_block(b64Text.c_str(), b64Text.length(), (char*)output, &s);
+  return cnt;
+}
+
+void config_certify() 
+{
+  
+  uint8_t binaryCert[AWS_CERT_CRT.length() * 3 / 4];
+  int len = b64decode(AWS_CERT_CRT, binaryCert);
+  espClient.setCertificate(binaryCert, len);
+  
+  uint8_t binaryPrivate[AWS_KEY_PRIVATE.length() * 3 / 4];
+  len = b64decode(AWS_KEY_PRIVATE, binaryPrivate);
+  espClient.setPrivateKey(binaryPrivate, len);
+
+  uint8_t binaryCA[AWS_CERT_CA.length() * 3 / 4];
+  len = b64decode(AWS_CERT_CA, binaryCA);
+  espClient.setCACert(binaryCA, len);
+}
+
+void getDate()// Não está sendo usado ainda
+{
+    //Recupera os dados de data e horário usando o client NTP
+    //String formattedTime = ntpClient.getFormattedTime();
+    String formattedTime = ntpClient.getFormattedDate();
+    Serial.print("Formato Hora e Data: ");
+    Serial.println(formattedTime);
+
+    unsigned long epochTime = ntpClient.getEpochTime();
+    struct tm *ptm = gmtime ((time_t *)&epochTime);
+    String date = String(ptm->tm_year+1900) + "-" + String(ptm->tm_mon+1) + "-" + String(ptm->tm_mday);
+    Serial.print("Data: ");
+    Serial.println(date);
+
+    String hour = String(ntpClient.getHours()) + ":" + String(ntpClient.getMinutes());
+    Serial.print("Horas: ");
+    Serial.println(hour); 
+    Serial.println(); 
 }
 
 void setup()
@@ -40,6 +143,8 @@ void setup()
  
   Serial.println();   // Pula uma linha na janela da serial
 
+  espClient.setBufferSizes(512, 512);
+
   config_wifi("/wifi_credential.txt");
   
   pinMode(2, OUTPUT);              
@@ -47,7 +152,8 @@ void setup()
   
   WiFi.begin(ssid, password); //Passa os parâmetros para a função que vai fazer a conexão com a rede sem fio
   delay(1000); // Intervalo de 1000 milisegundos
-  Serial.print("Conectando"); // Escreve um texto na serial
+  Serial.print("Conectando à "); // Escreve um texto na serial
+  Serial.println(ssid);
   
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -56,18 +162,74 @@ void setup()
   }
   
   Serial.println();
-  server.begin();  //Inicia o Servidor para receber dados na porta definida em "WiFiServer server(porta)"
+  Serial.println("WiFi conectado");
+  //server.begin();  //Inicia o Servidor para receber dados na porta definida em "WiFiServer server(porta)"
   Serial.print("Endereço IP: "); // Escreve na janela da serial
-  
   Serial.println(WiFi.localIP()); // Escreve na  o IP recebido dentro da rede sem fio (recebido de forma automática)
+
+  setupNTP();
   
   delay(1000); // Intervalo de 1000 milisegundos
 
+  config_certify();
+
+}
+
+void reconnect() 
+{
+  // Loop até estarmos reconectados
+  while (!client_pubsub.connected()) // Enquanto falhar a conexão
+  {
+    Serial.print("Tentando conexão MQTT AWS IoT...");
+    // Tentativa de conexão
+    if (client_pubsub.connect("ESPthing")) 
+    {
+      Serial.println("Conectado");
+      // Depois de conectado, publique uma aviso ...
+      client_pubsub.publish("outTopic", "hello world");
+      // ... e reinscrever
+      client_pubsub.subscribe("inTopic");
+    } 
+    else
+    {
+      Serial.print("falhou, rc=");
+      Serial.print(client_pubsub.state());
+      Serial.println(" tente novamente em 5 segundos");
+  
+      char buf[256];
+      espClient.getLastSSLError(buf, 256);
+      Serial.print("Erro de SSL de WiFiClientSecure: ");
+      Serial.println(buf);
+  
+      // Aguarde 5 segundos antes de tentar novamente
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
+
+  if (!client_pubsub.connected()) // Se não houver a conexão
+  {
+    reconnect(); // tenta reconectar
+  }
+  client_pubsub.loop();
   
-  WiFiClient client = server.available();  //Verifica se algum Cliente está conectado no Servidor   
+  long now = millis();
+  if (now-lastMsg > 5000) // se o tempo da mensagem atual menos o tempo da ultima mensagem ultrapassar os 5 segundos
+  {
+    lastMsg = now;
+    count_send++;
+    //crie a mensagem a ser enviada
+    snprintf (msg, 75, "{\"Message\": \"Energy Consumption\",\"value\": %d}", count_send); 
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+  
+    // publicar mensagens no tópico "outTopic"
+    client_pubsub.publish("outTopic", msg);  
+  }
+  
+  /*WiFiClient client = server.available();  //Verifica se algum Cliente está conectado no Servidor   
   
   if (!client) {  // Se não existir Cliente conectado, faz
     return; // Reexecuta o processo até que algum Cliente se conecte ao Servidor
@@ -119,4 +281,5 @@ void loop() {
  client.println("</html>");  //Fecha a tag "html"
  delay(1); //Intervalo de 1 milisegundo 
  Serial.println(""); //Pula uma linha na janela serial
+ */
 }
